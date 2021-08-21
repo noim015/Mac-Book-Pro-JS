701182e2a24bd2aa4f04876622db3afbd0ffc391
noim015 <sayed.noim.ahmed.com>
2021-08-21 13:06:58 +0600

    Storage section done

--- js/app.js
+++ js/app.js
@@ -24,3 +24,37 @@ document.getElementById('memory-16gb').addEventListener('click',function(){
     getExtraMemoryID.innerText = 180;
     
 })
+
+//Event Handler for 256GB SSD Storage
+document.getElementById('256gb-storage').addEventListener('click',function(){
+    const exMemoryCost = parseFloat(document.getElementById('extra-memory-cost').innerText);
+    const getExtraMemoryID = document.getElementById('extra-storage-cost');
+    const getExtraMemoryCost = parseFloat(getExtraMemoryID.innerText);
+    const totalPriceId = document.getElementById('total-price');
+    const totalPrice = parseFloat(totalPriceId.innerText);
+    totalPriceId.innerText = 0 + 1299 + exMemoryCost;
+    getExtraMemoryID.innerText = 0;
+    
+})
+//Event Handler for 512GB SSD Storage
+document.getElementById('512gb-storage').addEventListener('click',function(){
+    const exMemoryCost = parseFloat(document.getElementById('extra-memory-cost').innerText);
+    const getExtraMemoryID = document.getElementById('extra-storage-cost');
+    const getExtraMemoryCost = parseFloat(getExtraMemoryID.innerText);
+    const totalPriceId = document.getElementById('total-price');
+    const totalPrice = parseFloat(totalPriceId.innerText);
+    totalPriceId.innerText = 1299 + 100 +exMemoryCost;
+    getExtraMemoryID.innerText = 100;
+    
+})
+//Event Handler for 1TB SSD Storage
+document.getElementById('1tb-storage').addEventListener('click',function(){
+    const exMemoryCost = parseFloat(document.getElementById('extra-memory-cost').innerText);
+    const getExtraMemoryID = document.getElementById('extra-storage-cost');
+    const getExtraMemoryCost = parseFloat(getExtraMemoryID.innerText);
+    const totalPriceId = document.getElementById('total-price');
+    const totalPrice = parseFloat(totalPriceId.innerText);
+    totalPriceId.innerText = 1299 + 180+exMemoryCost;
+    getExtraMemoryID.innerText = 180;
+    
+})
