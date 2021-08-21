d9626277417edb46e449319612077a2f3aea113e
noim015 <sayed.noim.ahmed.com>
2021-08-21 13:26:54 +0600

    Delivery section done

--- js/app.js
+++ js/app.js
@@ -28,33 +28,62 @@ document.getElementById('memory-16gb').addEventListener('click',function(){
 //Event Handler for 256GB SSD Storage
 document.getElementById('256gb-storage').addEventListener('click',function(){
     const exMemoryCost = parseFloat(document.getElementById('extra-memory-cost').innerText);
-    const getExtraMemoryID = document.getElementById('extra-storage-cost');
-    const getExtraMemoryCost = parseFloat(getExtraMemoryID.innerText);
+    const exDeliveryCost = parseFloat(document.getElementById('delivery-charge').innerText);
+    const getExtraStorageID = document.getElementById('extra-storage-cost');
+    const getExtraStorageCost = parseFloat(getExtraStorageID.innerText);
     const totalPriceId = document.getElementById('total-price');
     const totalPrice = parseFloat(totalPriceId.innerText);
-    totalPriceId.innerText = 0 + 1299 + exMemoryCost;
-    getExtraMemoryID.innerText = 0;
+    totalPriceId.innerText = 0 + 1299 + exMemoryCost + exDeliveryCost;
+    getExtraStorageID.innerText = 0;
     
 })
 //Event Handler for 512GB SSD Storage
 document.getElementById('512gb-storage').addEventListener('click',function(){
     const exMemoryCost = parseFloat(document.getElementById('extra-memory-cost').innerText);
-    const getExtraMemoryID = document.getElementById('extra-storage-cost');
-    const getExtraMemoryCost = parseFloat(getExtraMemoryID.innerText);
+    const exDeliveryCost = parseFloat(document.getElementById('delivery-charge').innerText);
+    const getExtraStorageID = document.getElementById('extra-storage-cost');
+    const getExtraStorageCost = parseFloat(getExtraStorageID.innerText);
     const totalPriceId = document.getElementById('total-price');
     const totalPrice = parseFloat(totalPriceId.innerText);
-    totalPriceId.innerText = 1299 + 100 +exMemoryCost;
-    getExtraMemoryID.innerText = 100;
+    totalPriceId.innerText = 1299 + 100 + exMemoryCost + exDeliveryCost;
+    getExtraStorageID.innerText = 100;
     
 })
 //Event Handler for 1TB SSD Storage
 document.getElementById('1tb-storage').addEventListener('click',function(){
     const exMemoryCost = parseFloat(document.getElementById('extra-memory-cost').innerText);
-    const getExtraMemoryID = document.getElementById('extra-storage-cost');
-    const getExtraMemoryCost = parseFloat(getExtraMemoryID.innerText);
+    const exDeliveryCost = parseFloat(document.getElementById('delivery-charge').innerText);
+    const getExtraStorageID = document.getElementById('extra-storage-cost');
+    const getExtraStorageCost = parseFloat(getExtraStorageID.innerText);
     const totalPriceId = document.getElementById('total-price');
     const totalPrice = parseFloat(totalPriceId.innerText);
-    totalPriceId.innerText = 1299 + 180+exMemoryCost;
-    getExtraMemoryID.innerText = 180;
+    totalPriceId.innerText = 1299 + 180 + exMemoryCost + exDeliveryCost;
+    getExtraStorageID.innerText = 180;
+    
+})
+
+//Event Handler for Free Delivery
+document.getElementById('free-delivery').addEventListener('click',function(){
+    const exMemoryCost = parseFloat(document.getElementById('extra-memory-cost').innerText);
+    const exStorageCost = parseFloat(document.getElementById('extra-storage-cost').innerText);
+    const getExtraDeliveryID = document.getElementById('delivery-charge');
+    const getExtraDeliveryCost = parseFloat(getExtraDeliveryID.innerText);
+    const totalPriceId = document.getElementById('total-price');
+    const totalPrice = parseFloat(totalPriceId.innerText);
+    totalPriceId.innerText = 0 + 1299 + exMemoryCost + exStorageCost;
+    getExtraDeliveryID.innerText = 0;
+    
+})
+//Event Handler for Paid Delivery
+document.getElementById('paid-delivery').addEventListener('click',function(){
+    const exMemoryCost = parseFloat(document.getElementById('extra-memory-cost').innerText);
+    const exStorageCost = parseFloat(document.getElementById('extra-storage-cost').innerText);
+    const getExtraDeliveryID = document.getElementById('delivery-charge');
+    const getExtraDeliveryCost = parseFloat(getExtraDeliveryID.innerText);
+    const totalPriceId = document.getElementById('total-price');
+    const totalPrice = parseFloat(totalPriceId.innerText);
+    totalPriceId.innerText = 20 + 1299 + exMemoryCost + exStorageCost;
+    getExtraDeliveryID.innerText = 20;
+   
     
 })
